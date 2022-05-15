0ea492f445a7cb121411609fb5fe94b008be2e10
Akash Chanda Tushar <akash.tushar98@gmail.com>
2022-05-15 16:21:59 +0600

    footer completed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,32 @@
-import React from "react";
+import footer from "../assets/images/footer.png";
 
 const Footer = () => {
-    return <div>this is a Footer</div>;
+    return (
+        <footer>
+            <div class="footer p-10  " style={{ background: `url(${footer})` }}>
+                <div>
+                    <span class="footer-title">Services</span>
+                    <a class="link link-hover">Emergency Checkup</a>
+                    <a class="link link-hover">Monthly Checkup</a>
+                    <a class="link link-hover">Weekly Checkup</a>
+                    <a class="link link-hover">deleniti Checkup</a>
+                </div>
+                <div>
+                    <span class="footer-title">Our Health</span>
+                    <a class="link link-hover">Cavity Filling</a>
+                    <a class="link link-hover">Teath Whitening</a>
+                    <a class="link link-hover">Fluoride Treatment</a>
+                </div>
+                <div>
+                    <span class="footer-title">Our Address</span>
+                    <p class="">New York - 101010 Hudson</p>
+                </div>
+            </div>
+            <div className="text-center">
+                <p>Copyright © 2022 - All right reserved by Doctors Portal</p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
